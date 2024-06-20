40907b6b16fc13969d451cc6cddab9f2e3ee34da
wang4996 <wang4996@purdue.edu>
2024-06-19 23:26:35 -0500

    script addition

--- script/run.sh
+++ script/run.sh
@@ -12,39 +12,42 @@ readonly=(100 0 0 0 0)
 updateonly=(0 0 100 0 0)
 
 #exp
-threads=(0 2 18 36 72 108 144)
-#threads=(0 2 16 32 64 96 128)
+#threads=(0 2 18 36 72 108 144)
+threads=(0 2 16 32 64 96 128)
 mem_threads=(0 4)
-cache=(0 64 128 256 512 1024)
+cache=(0 64 128 256 512 1024 2048 8192)
 uniform=(0 1)
 zipf=(0.99)
 bulk=50
 warmup=10
 runnum=50
-nodenum=2
+nodenum=8
 
 #other
 correct=0
 timebase=1
 early=1
 #index=(0 1 2)
-rpc=1
+rpc=0
 admit=0.1
 tune=0
 
-
-for uni in 0
+./hugepage.sh
+for uni in (0 1)
 do 
-    for op in 0
+    for op in (0 1 2)
     do 
         for idx in 0
-        do  
-            for t in 1
+        do
+          for cache_size in (6 7)
+                  do
+            for t in 4
             do
                 ./restartMemc.sh
-                sudo ./newbench $nodenum ${read[$op]} ${insert[$op]} ${update[$op]} ${delete[$op]} ${range[$op]} ${threads[$t]} ${mem_threads[1]} ${cache[3]} $uni ${zipf[0]} $bulk $warmup $runnum $correct $timebase $early $idx $rpc $admit $tune 36
+                sudo ./newbench $nodenum ${read[$op]} ${insert[$op]} ${update[$op]} ${delete[$op]} ${range[$op]} ${threads[$t]} ${mem_threads[0]} ${cache[$cache_size]} $uni ${zipf[0]} $bulk $warmup $runnum $correct $timebase $early $idx $rpc $admit $tune 36
                 sleep 2
             done
+          done
         done
     done
 done
---
+++ script/sync_all.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+home_dir="/users/Ruihong/dex/"
+nmemory="10"
+ncompute="10"
+nmachines="20"
+nshard="10"
+numa_node=("0" "1")
+port=$((10000+RANDOM%1000))
+bin=`dirname "$0"`
+bin=`cd "$bin"; pwd`
+SRC_HOME=$bin/..
+BIN_HOME=$bin/../release
+core_dump_dir="/mnt/core_dump"
+function run_bench() {
+  communication_port=()
+#	memory_port=()
+	memory_server=()
+  memory_shard=()
+
+#	compute_port=()
+	compute_server=()
+	compute_shard=()
+#	machines=()
+	i=0
+  n=0
+  while [ $n -lt $nmemory ]
+  do
+    memory_server+=("node-$i")
+    i=$((i+1))
+    n=$((n+1))
+  done
+  n=0
+  i=$((nmachines-1))
+  while [ $n -lt $ncompute ]
+  do
+
+    compute_server+=("node-$i")
+    i=$((i-1))
+    n=$((n+1))
+  done
+  echo "here are the sets up"
+  echo $?
+  echo compute servers are ${compute_server[@]}
+  echo memoryserver is ${memory_server[@]}
+#  echo ${machines[@]}
+  n=0
+  while [ $n -lt $nshard ]
+  do
+    communication_port+=("$((port+n))")
+    n=$((n+1))
+  done
+  n=0
+  while [ $n -lt $nshard ]
+  do
+    # if [[ $i == "2" ]]; then
+    # 	i=$((i-1))
+    # 	continue
+    # fi
+    compute_shard+=(${compute_server[$n%$ncompute]})
+    memory_shard+=(${memory_server[$n%$nmemory]})
+    n=$((n+1))
+  done
+  echo compute shards are ${compute_shard[@]}
+  echo memory shards are ${memory_shard[@]}
+  echo communication ports are ${communication_port[@]}
+#  test for download and compile the codes
+  n=0
+
+  for node in ${memory_shard[@]}
+  do
+    echo "Rsync the $node rsync -a $home_dir $node:$home_dir"
+#    ssh -o StrictHostKeyChecking=no $node "sudo apt-get install -y libnuma-dev numactl htop libmemcached-dev libboost-all-dev" &
+    rsync -a $home_dir $node:$home_dir
+    ssh -o StrictHostKeyChecking=no $node "pkill -f newbench" &
+    ssh -o StrictHostKeyChecking=no $node "rm $home_dir/scripts/log*" &
+    ssh ${ssh_opts} $node "echo '$core_dump_dir/core$compute' | sudo tee /proc/sys/kernel/core_pattern" &
+  done
+
+  for node in ${compute_shard[@]}
+  do
+    echo "Rsync the $node rsync -a $home_dir $node:$home_dir"
+#    ssh -o StrictHostKeyChecking=no $node "sudo apt-get install -y libnuma-dev numactl htop libmemcached-dev libboost-all-dev" &
+    rsync -a $home_dir $node:$home_dir
+    ssh -o StrictHostKeyChecking=no $node "pkill -f newbench" &
+    ssh -o StrictHostKeyChecking=no $node "rm $home_dir/scripts/log*" &
+    ssh ${ssh_opts} $node "echo '$core_dump_dir/core$compute' | sudo tee /proc/sys/kernel/core_pattern" &
+
+
+
+  done
+  read -r -a memcached_node <<< $(head -n 1 $SRC_HOME/memcached_ip.conf)
+  echo "restart memcached on ${memcached_node[0]}"
+  ssh -o StrictHostKeyChecking=no ${memcached_node[0]} "sudo service memcached restart"
+
+
+	}
+	run_bench
