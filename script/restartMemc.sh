0745e2a6180cc819a595ef41265df12d6b3fd5ac
wang4996 <wang4996@purdue.edu>
2024-06-20 14:50:13 -0500

    comilaton error fix

--- script/restartMemc.sh
+++ script/restartMemc.sh
@@ -2,12 +2,14 @@
 addr=$(head -1 ../memcached.conf)
 port=$(awk 'NR==2{print}' ../memcached.conf)
 
-# kill old me
-ssh  ${addr} "cat /tmp/memcached.pid | xargs kill"
-
-# launch memcached
-ssh ${addr} "memcached -u root -l ${addr} -p  ${port} -c 10000 -d -P /tmp/memcached.pid"
+## kill old me
+#ssh  ${addr} "cat /tmp/memcached.pid | xargs kill"
+#
+## launch memcached
+#ssh ${addr} "memcached -u root -l ${addr} -p  ${port} -c 10000 -d -P /tmp/memcached.pid"
+ssh -o StrictHostKeyChecking=no ${addr} "sudo service memcached restart"
 sleep 1
+
 
 # init
 RET=$(echo -e "set serverNum 0 0 1\r\n0\r\nquit\r" | nc ${addr} ${port})
@@ -19,11 +21,12 @@ fi
 addr=$(head -1 ../memcached.conf)
 port=$(awk 'NR==2{print}' ../memcached.conf)
 
-# kill old me
-ssh  ${addr} "cat /tmp/memcached.pid | xargs kill"
-
-# launch memcached
-ssh ${addr} "memcached -u root -l ${addr} -p  ${port} -c 10000 -d -P /tmp/memcached.pid"
+## kill old me
+#ssh  ${addr} "cat /tmp/memcached.pid | xargs kill"
+#
+## launch memcached
+#ssh ${addr} "memcached -u root -l ${addr} -p  ${port} -c 10000 -d -P /tmp/memcached.pid"
+ssh -o StrictHostKeyChecking=no ${addr} "sudo service memcached restart"
 sleep 1
 
 # init
