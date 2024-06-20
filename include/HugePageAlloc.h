6dac0a0c4ab6c4cc7f5962bbf38de2d35a61b585
wang4996 <wang4996@purdue.edu>
2024-06-20 12:04:54 -0500

    comilaton error fix

--- include/HugePageAlloc.h
+++ include/HugePageAlloc.h
@@ -9,13 +9,13 @@
 
 char *getIP();
 inline void *hugePageAlloc(size_t size) {
-  numa_set_preferred(0);
+//  numa_set_preferred(0);
   void *res = mmap(NULL, size, PROT_READ | PROT_WRITE,
                    MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
   if (res == MAP_FAILED) {
     Debug::notifyError("%s mmap failed!\n", getIP());
   }
-  numa_set_localalloc();
+//  numa_set_localalloc();
   return res;
 }
 
