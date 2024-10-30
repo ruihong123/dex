1633e49f7c278df72be890daa596174c41d97b91
ruihong123 <wang4996@purdue.edu>
2024-10-30 14:12:42 -0400

    install tbb

--- script/sync_all.sh
+++ script/sync_all.sh
@@ -72,7 +72,7 @@ function run_bench() {
 #    ssh -o StrictHostKeyChecking=no $node "sudo apt-get install -y libnuma-dev numactl htop libmemcached-dev libboost-all-dev" &
     rsync -a $home_dir $node:$home_dir
     ssh -o StrictHostKeyChecking=no $node "pkill -f newbench" &
-#    ssh -o StrictHostKeyChecking=no $node "sudo apt install libtbb-dev -y" &
+    ssh -o StrictHostKeyChecking=no $node "sudo apt install libtbb-dev -y" &
 
     ssh -o StrictHostKeyChecking=no $node "rm $home_dir/scripts/log*" &
     ssh ${ssh_opts} $node "echo '$core_dump_dir/core$compute' | sudo tee /proc/sys/kernel/core_pattern" &
@@ -84,7 +84,7 @@ function run_bench() {
 #    ssh -o StrictHostKeyChecking=no $node "sudo apt-get install -y libnuma-dev numactl htop libmemcached-dev libboost-all-dev" &
     rsync -a $home_dir $node:$home_dir
     ssh -o StrictHostKeyChecking=no $node "pkill -f newbench" &
-#    ssh -o StrictHostKeyChecking=no $node "sudo apt install libtbb-dev -y" &
+    ssh -o StrictHostKeyChecking=no $node "sudo apt install libtbb-dev -y" &
 
     ssh -o StrictHostKeyChecking=no $node "rm $home_dir/scripts/log*" &
     ssh ${ssh_opts} $node "echo '$core_dump_dir/core$compute' | sudo tee /proc/sys/kernel/core_pattern" &
