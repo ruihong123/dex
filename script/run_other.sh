c2ecd8645eccbe13bdb329ec09ea7d7a7005cd00
ruihong123 <wang4996@purdue.edu>
2024-10-30 14:53:43 -0400

    install tbb

--- script/run_other.sh
+++ script/run_other.sh
@@ -45,7 +45,7 @@ do
                   do
             for t in 4
             do
-                ./restartMemc.sh
+#                ./restartMemc.sh
                 ulimit -c 38000000
                 sudo ../build/newbench $nodenum ${read[$op]} ${insert[$op]} ${update[$op]} ${delete[$op]} ${range[$op]} ${threads[$t]} ${mem_threads[1]} ${cache[$cache_size]} $uni ${zipf[0]} $bulk $warmup $runnum $correct $timebase $early $idx $rpc $admit $tune 8
                 sleep 2
